389fe7c56e630f4a97270eae3533ddd91f9515e2
think <think@animation.zte.com.cn>
2015-12-31 22:51:55 +0200

    new branch

---
+++ groupOrganizer/src/Connector/MGConnection.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Predis groupOrganizer.
+ * 
+ * (c) Osama Agha <osama.agha24@gmail.com>
+ * and open the template in the editor.
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GOrganizer\Connector;
+
+use \Predis\Client;
+use \GOrganizer\Configuration\Config;
+
+/**
+ * 
+ * @author Osama Agha <osama.agha24@gmail.com>
+ */
+class GConnection
+{
+    public function getConnection()
+    {
+        $connection = new Client(Config::redisHost());
+        return $connection;
+    }
+
+}
